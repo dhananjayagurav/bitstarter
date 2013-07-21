a21f9d6c55243966479cf3c211b5db947d017fe2
Dhananjay Gurav <dhananjay.a.gurav@gmail.com>
2013-07-21 16:33:37 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -6,11 +6,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var buf = new Buffer (fs.readFileSync('index.html'));
-response.send(buf.toString()); 
+ 
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-//  response.send(buf.toString());  
+  response.send(buf.toString());  
 });
 
 var port = process.env.PORT || 5000;
